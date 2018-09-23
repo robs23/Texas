2f520b1c351c720b3ce3e1b1d5b8cbc08edf08eb
robert roszak <robert.roszak@outlook.com>
2018-09-23 14:23:39 +0200

    Localization for EN & PL implemented

--- Texas/Startup.cs
+++ Texas/Startup.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +24,11 @@ namespace Texas
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            services.AddMvc()
+              .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix)
+              .AddDataAnnotationsLocalization();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +43,18 @@ namespace Texas
             {
                 app.UseExceptionHandler("/Error");
             }
+
+            var supportedCultures = new[]
+            {
+                new CultureInfo("en-US"),
+                new CultureInfo("pl-PL")
+            };
+            app.UseRequestLocalization(new RequestLocalizationOptions
+            {
+                DefaultRequestCulture = new RequestCulture("en-US"),
+                SupportedCultures = supportedCultures,
+                SupportedUICultures = supportedCultures
+            });
 
             app.UseStaticFiles();
 
--- Texas/wwwroot/js/site.js
+++ Texas/wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your Javascript code.
+$(document).ready(function () {
+    $('.smooth-slide').click(function (e) {
+        var linkHref = $(this).attr('href');
+
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 1000);
+
+        e.preventDefault();
+    });
+});
