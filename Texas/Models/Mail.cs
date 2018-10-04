8bd3ed7b5de9c74a4f35597954f0c7b07ac57ea5
robert roszak <robert.roszak@outlook.com>
2018-10-04 09:33:03 +0200

    Contact Me with basic validation works

---
+++ Texas/Models/Mail.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Threading.Tasks;
+
+namespace Texas.Models
+{
+    public class Mail
+    {
+        SmtpClient Smtp;
+        string Status = "";
+        MailMessage Email;
+
+        public Mail()
+        {
+            //setting up gmail server
+
+            Email = new MailMessage();
+            Smtp = new SmtpClient();
+            Smtp.Host = "poczta.webserwer.pl";
+            Smtp.EnableSsl = true;
+            Smtp.Port = 587;//or 465 or 25 or 587
+            Smtp.UseDefaultCredentials = false;
+            var ConfManager = Static.Secrets.NoReplyPassword;
+            Smtp.Credentials = new System.Net.NetworkCredential("no-reply@systo.pl", ConfManager);
+            Smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+        }
+        public void Send(string inputEmail, string subject, string textBody, string htmlBody)
+        {
+            try
+            {
+                //creating an email
+
+                MailAddress fromAddress = new MailAddress("no-reply@systo.pl", "systo.pl");
+                this.Email.From = fromAddress;
+                this.Email.To.Add(inputEmail);
+                this.Email.Subject = subject;
+                this.Email.Body = textBody;
+                ContentType mimeType = new ContentType("text/html");
+                AlternateView alternate = AlternateView.CreateAlternateViewFromString(htmlBody, mimeType);
+                this.Email.AlternateViews.Add(alternate);
+                Smtp.Send(this.Email);
+                this.Status = "Wysyłanie wiadomości zakończone sukcesem";
+            }
+            catch (Exception ex)
+            {
+                this.Status = "Error: " + ex.ToString();
+            }
+        }
+    }
+}
--- Texas/Pages/Index.cshtml.cs
+++ Texas/Pages/Index.cshtml.cs
@@ -1,17 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Texas.Models;
 
 namespace Texas.Pages
 {
     public class IndexModel : PageModel
     {
+        [BindProperty]
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+        [BindProperty]
+        [Required]
+        [MinLength(4)]
+        public string Mail { get; set; }
+        [BindProperty]
+        [Required]
+        [MinLength(5)]
+        public string Message { get; set; }
+
         public void OnGet()
         {
 
+        }
+
+        //public IActionResult OnPost()
+        //{
+        //    Name = Request.Form["Name"];
+        //    Mail mail = new Mail();
+        //    string textBody = "To jest textbody";
+        //    string htmlBody = "<p><b>To jest htmlBody</b></p>";
+        //    mail.Send("robert.roszak@gmail.com", "Nowa wiadomość na Twojej stronie!", textBody, htmlBody);
+        //    return new JsonResult("Received at " + DateTime.Now);
+        //}
+
+        public IActionResult OnPostSendMailAsync()
+        {
+            //Name = Request.Form["Name"];
+            Mail mail = new Mail();
+            string textBody =  Name + " przesyła wiadomość: " + Message + ". Możesz odpowiedzieć na adres: " + Mail;
+            string htmlBody = string.Format("<p><b>{0}</b> przesyła wiadomość: <br><br>{1}<br><br>Możesz odpowiedzieć na adres: {2}</p>",Name,Message,Mail);
+            mail.Send(Static.Secrets.ContactMeMail, "Nowa wiadomość na Twojej stronie!", textBody, htmlBody);
+            return RedirectToPage();
         }
     }
 }
