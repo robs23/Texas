040c3313c755912aa06230f9ef5627f9f44a8ce3
robs23 <robert.roszak@gmail.com>
2019-11-14 19:12:21 +0100

    Trying to get domain name from asp.net razor pages routing the proper way..

--- Texas/Pages/Projects/Systo.cshtml.cs
+++ Texas/Pages/Projects/Systo.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,6 +10,20 @@ namespace Texas.Pages
 {
     public class SystoModel : PageModel
     {
+        public readonly IHostingEnvironment hostingEnvironment;
+        public string WebRootPath
+        {
+            get
+            {
+                return hostingEnvironment.WebRootPath;
+            }
+        }
+
+        public SystoModel(IHostingEnvironment environment)
+        {
+            this.hostingEnvironment = environment;
+        }
+
         public void OnGet()
         {
 
