fef2f6262e3c4f3532c6d948776bb6ea7ac28231
robs23 <robert.roszak@gmail.com>
2019-11-15 19:18:46 +0100

    Implementing featureImg js class to handle .feature-img elements' state. The goal is to display gif once appropriate element gets into view

--- Texas/Pages/Projects/Systo.cshtml.cs
+++ Texas/Pages/Projects/Systo.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -26,7 +28,7 @@ namespace Texas.Pages
 
         public void OnGet()
         {
-
+            //var host = Request.Host.Value;
         }
     }
 }
---
+++ Texas/wwwroot/js/feature-img.js
@@ -0,0 +1,7 @@
+﻿class FeatureImg{
+    constructor(id, isDownloaded, isDisplayed) {
+        this.id = id;
+        this.isDisplayed = isDisplayed;
+        this.isDownloaded = isDownloaded;
+    }
+}
