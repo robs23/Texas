6d4089e68bb9ed080b82d637cf0fed61c85cbc0d
robs23 <robert.roszak@gmail.com>
2019-11-16 18:22:51 +0100

    Replacing PNG with GIF when  GIF is downloaded and PNG is fully displayed is now working

--- Texas/wwwroot/js/feature-img.js
+++ Texas/wwwroot/js/feature-img.js
@@ -1,7 +1,25 @@
-﻿class FeatureImg{
-    constructor(id, isDownloaded, isDisplayed) {
+﻿
+class FeatureImg{
+    id = "";
+    isDisplayed = false;
+    isDownloaded = false;
+    image = null;
+
+    constructor(id, isDownloaded, isDisplayed, image) {
         this.id = id;
         this.isDisplayed = isDisplayed;
         this.isDownloaded = isDownloaded;
+        this.image = image;
+        this.imageLoaded = this.imageLoaded.bind(this);
+    }
+
+    imageLoaded() {
+        this.isDownloaded = true;
+    }
+
+    download() {
+        var downloadingImage = new Image();
+        downloadingImage.addEventListener('load', this.imageLoaded);
+        downloadingImage.src = "/images/Async/"+ this.id +".gif";
     }
 }
