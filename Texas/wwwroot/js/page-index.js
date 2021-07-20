3797e9286ed9ff3a4e33a5665dbd35f30284748f
Robert Roszak <robert.roszak@outlook.com>
2021-07-20 21:14:00 +0200

    Animations working for bio elements

--- Texas/wwwroot/js/page-index.js
+++ Texas/wwwroot/js/page-index.js
@@ -2,11 +2,47 @@
 
 window.addEventListener('scroll', function () {
     var fadable = this.document.getElementsByClassName("fadable");
+    var slidableFromLeft = this.document.getElementsByClassName("slidable-from-left");
+    var slidableFromRight = this.document.getElementsByClassName("slidable-from-right");
 
     for (var i = 0; i < fadable.length; i++) {
         var isElementInView = utils.isElementInView($('#' + fadable[i].id), true);
         if (isElementInView) {
-            fadable[i].classList.add("fade-in");
+            if (!fadable[i].style.animation.includes("fade-in")){
+                fadable[i].style.animation = "fade-in 0.5s ease-in forwards";
+                fadable[i].style.visibility = "visible";
+            }
+            //fadable[i].classList.add("fade-in");
+        }
+    }
+
+    for(var i = 0; i < slidableFromLeft.length; i++) {
+        isElementInView = utils.isElementInView($('#' + slidableFromLeft[i].id), true);
+        if (isElementInView) {
+            var prevAnimation;
+            if (slidableFromLeft[i].style.animation) {
+                //there's already some value, append
+                prevAnimation = slidableFromLeft[i].style.animation + ", ";
+            }
+            if (!slidableFromLeft[i].style.animation.includes("slide-from-left")){
+                slidableFromLeft[i].style.animation = prevAnimation + "slide-from-left 0.4s ease-in forwards";
+            }
+            
+        }
+    }
+
+    for (var i = 0; i < slidableFromRight.length; i++) {
+        isElementInView = utils.isElementInView($('#' + slidableFromRight[i].id), true);
+        if (isElementInView) {
+            prevAnimation;
+            if (slidableFromRight[i].style.animation) {
+                //there's already some value, append
+                prevAnimation = slidableFromRight[i].style.animation + ", ";
+            }
+            if (!slidableFromRight[i].style.animation.includes("slide-from-right")) {
+                slidableFromRight[i].style.animation = prevAnimation + "slide-from-right 0.4s ease-in forwards";
+            }
+
         }
     }
 });
