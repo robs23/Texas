12df5b0927d6cfdce43e2085a799226276487306
Robert Roszak <robert.roszak@outlook.com>
2021-07-20 14:20:18 +0200

    Playing around with animations to appear only when its node is dispalyed on screen

---
+++ Texas/wwwroot/js/page-index.js
@@ -0,0 +1,12 @@
+﻿var utils = new Utils();
+
+window.addEventListener('scroll', function () {
+    var fadable = this.document.getElementsByClassName("fadable");
+
+    for (var i = 0; i < fadable.length; i++) {
+        var isElementInView = utils.isElementInView($('#' + fadable[i].id), true);
+        if (isElementInView) {
+            fadable[i].classList.add("fade-in");
+        }
+    }
+});
